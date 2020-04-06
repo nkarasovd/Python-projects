0911662e37bcd611a66b355f9bb65076b90a9e10
Nikolai <dancingdar@mail.ru>
2020-04-06 14:00:40 +0300

    week4 completed

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,52 +1,90 @@
 # Uses python3
 import sys
+import random
 
 
-class Point:
-    def __init__(self, value, index):
-        self.value = value
-        self.index = index
+def less_than_or_equal(num1, num2, let1, let2):
+    return less_than(num1, num2, let1, let2) or \
+           equal(num1, num2, let1, let2)
 
-    def get_index(self):
-        return self.index
 
-    def get_value(self):
-        return self.value
+def less_than(num1, num2, let1, let2):
+    return num1 < num2 or \
+           (num1 == num2 and let1 < let2)
+
+
+def equal(num1, num2, let1, let2):
+    return num1 == num2 and let1 == let2
 
 
 def fast_count_segments(starts, ends, points):
-
-    points_classes = [Point(points[i], i) for i in range(len(points))]
-
-    points_classes.sort(key=lambda x: (x.get_value()))
-
     cnt = [0] * len(points)
-    starts.sort()
-    ends.sort()
+    LEFT = 1
+    POINT = 2
+    RIGHT = 3
 
-    min_start = 0
-    for el in points_classes:
-        if el.get_value() >= starts[min_start]:
-            while min_start < len(starts) and el.get_value() >= starts[min_start]:
-                min_start += 1
-            min_start -= 1
+    starts_l = [LEFT] * len(starts)
+    ends_r = [RIGHT] * len(ends)
+    points_p = [POINT] * len(points)
 
-            second_start = min_start
-            if el.get_value() <= ends[second_start]:
-                while second_start >= 0 and el.get_value() <= ends[second_start]:
-                    second_start -= 1
-                    cnt[el.get_index()] += 1
+    pairs_number = starts + ends + points
+    pairs_letter = starts_l + ends_r + points_p
 
-    return cnt
+    randomized_quick_sort(pairs_number, pairs_letter, 0, len(pairs_number) - 1)
 
+    count_left = 0
 
-def naive_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
+    point_counts = {}
+    for p in points:
+        point_counts[p] = 0
+
+    for i in range(len(pairs_number)):
+        if pairs_letter[i] == LEFT:
+            count_left += 1
+        elif pairs_letter[i] == RIGHT:
+            count_left -= 1
+        elif pairs_letter[i] == POINT:
+            if point_counts[pairs_number[i]] == 0:
+                point_counts[pairs_number[i]] += count_left
+
     for i in range(len(points)):
-        for j in range(len(starts)):
-            if starts[j] <= points[i] <= ends[j]:
-                cnt[i] += 1
+        cnt[i] = point_counts[points[i]]
+
     return cnt
+
+
+def partition3(a, b, l, r):
+    x = a[l]
+    letx = b[l]
+    begin = l + 1
+    end = l
+
+    for i in range(l + 1, r + 1):
+        if less_than_or_equal(a[i], x, b[i], letx):
+            end += 1
+            a[i], a[end] = a[end], a[i]
+            b[i], b[end] = b[end], b[i]
+            if less_than(a[end], x, b[end], letx):
+                a[begin], a[end] = a[end], a[begin]
+                b[begin], b[end] = b[end], b[begin]
+                begin += 1
+
+    a[l], a[begin - 1] = a[begin - 1], a[l]
+    b[l], b[begin - 1] = b[begin - 1], b[l]
+
+    return [begin, end]
+
+
+def randomized_quick_sort(a, b, l, r):
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    b[l], b[k] = b[k], b[l]
+    # use partition3
+    [m1, m2] = partition3(a, b, l, r)
+    randomized_quick_sort(a, b, l, m1 - 1);
+    randomized_quick_sort(a, b, m2 + 1, r);
 
 
 if __name__ == '__main__':
--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/week4_divide_and_conquer/6_closest_points/closest.py
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/week4_divide_and_conquer/6_closest_points/closest.py
@@ -1,10 +1,118 @@
-#Uses python3
+# Uses python3
 import sys
 import math
 
+"""
+Идея: разбиваем на две части, ищем минимум в каждой, потом ищем минимум в полосе.
+Причем в полосе смотрим только 7 ближайших точек:
+
+Итак, пусть мы рассматриваем какую-то точку p_i;
+напомним, что множество C(p_i) — это множество точек, y-координата которых лежит в отрезке [y_i-h; y_i],
+а, кроме того, по координате x и сама точка p_i, и все точки множества C(p_i) лежат в полосе шириной 2h.
+Иными словами, рассматриваемые нами точки p_i и C(p_i) лежат в прямоугольнике размера 2h * h.
+
+Наша задача — оценить максимальное количество точек, которое может лежать в этом прямоугольнике 2h * h;
+тем самым мы оценим и максимальный размер множества C(p_i). При этом при оценке надо не забывать,
+что могут встречаться повторяющиеся точки.
+
+Вспомним, что h получалось как минимум из двух результатов рекурсивных вызовов — от множеств A_1 и A_2,
+причём A_1 содержит точки слева от линии раздела и частично на ней,
+A_2 — оставшиеся точки линии раздела и точки справа от неё. Для любой пары точек из A_1, равно как и из A_2,
+расстояние не может оказаться меньше h — иначе бы это означало некорректность работы рекурсивной функции.
+
+Для оценки максимального количества точек в прямоугольнике 2h * h разобьём его на два квадрата h * h,
+к первому квадрату отнесём все точки C(p_i) пересечь с A_1, а ко второму — все остальные,
+т.е. C(p_i) пересечь с A_2. Из приведённых выше соображений следует,
+что в каждом из этих квадратов расстояние между любыми двумя точками не менее h.
+
+Покажем, что в каждом квадрате не более четырёх точек. Например, это можно сделать следующим образом:
+разобьём квадрат на 4 подквадрата со сторонами h/2. Тогда в каждом из этих подквадратов
+не может быть больше одной точки (т.к. даже диагональ равна h / \sqrt{2}, что меньше h).
+Следовательно, во всём квадрате не может быть более 4 точек.
+
+Итак, мы доказали, что в прямоугольнике 2h * h не может быть больше 4 * 2 = 8 точек, а, следовательно,
+размер множества C(p_i) не может превосходить 7, что и требовалось доказать."""
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return str([self.x, self.y])
+
+
+def distance(p1, p2):
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+
+def construct_points(x, y):
+    points = []
+    for i in range(len(x)):
+        points.append(Point(x[i], y[i]))
+
+    return points
+
+
 def minimum_distance(x, y):
-    #write your code here
-    return 10 ** 18
+    points = construct_points(x, y)
+    sorted_p_x = sorted(points, key=lambda p: p.x)
+
+    return large_size_min_distance(sorted_p_x)
+
+
+def small_size_min_distance(points):
+    result = sys.maxsize
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            result = min(result, distance(points[i], points[j]))
+    return result
+
+
+def large_size_min_distance(p_x):
+    size = len(p_x)
+    half_size = size // 2
+
+    if size <= 3:
+        return small_size_min_distance(p_x)
+
+    left_p_x = p_x[:half_size]
+    right_p_x = p_x[half_size:]
+
+    left_min = large_size_min_distance(left_p_x)
+    right_min = large_size_min_distance(right_p_x)
+
+    separated_min = min(left_min, right_min)
+
+    line_l = (left_p_x[-1].x + right_p_x[0].x) / 2
+
+    hybrid_min = compute_hybrid_min(left_p_x, right_p_x, line_l, separated_min)
+
+    return min(separated_min, hybrid_min)
+
+
+def compute_hybrid_min(left_x, right_x, line_l, wide):
+    left = []
+    for i in range(len(left_x)):
+        if abs(left_x[i].x - line_l) <= wide:
+            left.append(left_x[i])
+    right = []
+    for i in range(len(right_x)):
+        if abs(right_x[i].x - line_l) <= wide:
+            right.append(right_x[i])
+    total = left + right
+
+    total = sorted(total, key=lambda p: p.y)
+
+    result = wide
+    for i in range(len(total)):
+        for j in range(i + 1, min(i + 8, len(total))):
+            if abs(total[i].y - total[j].y) <= wide:
+                result = min(result, distance(total[i], total[j]))
+
+    return result
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
