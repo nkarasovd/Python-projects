4afe4cd33c91a6ed301d842563c93e7bf72dda1f
Nikolai <dancingdar@mail.ru>
2020-04-17 23:00:04 +0300

    week2 completed

--- Data Structures and Algorithms Specialization/Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ Data Structures and Algorithms Specialization/Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -19,10 +19,24 @@ class Database:
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        if self.ranks[src_parent] >= self.ranks[dst_parent]:
+            self.parents[dst_parent] = src_parent
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+            if self.max_row_count < self.row_counts[src_parent]:
+                self.max_row_count = self.row_counts[src_parent]
+            if self.ranks[dst_parent] == self.ranks[src_parent]:
+                self.ranks[src_parent] += 1
+        else:
+            self.parents[src_parent] = self.parents[dst_parent]
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+            if self.max_row_count < self.row_counts[dst_parent]:
+                self.max_row_count = self.row_counts[dst_parent]
         return True
 
     def get_parent(self, table):
         # find parent and compress path
+        if self.parents[table] != table:
+            self.parents[table] = self.get_parent(self.parents[table])
         return self.parents[table]
 
 
